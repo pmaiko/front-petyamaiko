7f64e6bbd6ab6a241ea1de011cd1c2c8b816db81
petyamaiko <petyamaiko@gmail.com>
2022-09-19 13:37:32 +0300

    init

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import './assets/styles/index.scss'
 
-import {RouterProvider} from 'react-router-dom'
+import { RouterProvider } from 'react-router-dom'
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import Default from './components/layout/default'
+
 import router from './plugins/router'
-// import reportWebVitals from './reportWebVitals';
+
+import reportWebVitals from './reportWebVitals'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
@@ -17,4 +18,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+reportWebVitals()
--- src/plugins/router.tsx
+++ src/plugins/router.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter} from 'react-router-dom'
+import { createBrowserRouter } from 'react-router-dom'
 
 import Home from '../components/pages/Home'
 import About from '../components/pages/About'
