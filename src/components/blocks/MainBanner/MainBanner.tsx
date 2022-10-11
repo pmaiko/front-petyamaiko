5870c40532bf3c6c7adfc9df1d438b1cae5193a8
petyamaiko <petyamaiko@gmail.com>
2022-10-11 20:57:36 +0300

    main

--- src/components/blocks/MainBanner/MainBanner.tsx
+++ src/components/blocks/MainBanner/MainBanner.tsx
@@ -2,36 +2,29 @@ import './MainBanner.scss'
 
 
 // @ts-ignore
-import img from '~/assets/images/petyamaiko.png'
-import svg from '~/assets/svg/code.svg'
+import img from '~/assets/images/petya.png'
+// import svg from '~/assets/svg/code.svg'
 import BaseButton from '~/components/base/BaseButton'
+import BaseImage from '~/components/base/BaseImage'
 
 const MainBanner = () => {
   return (
     <section className='main-banner'>
       <div className='container'>
-        <article className='main-banner__content'>
+        <div className='main-banner__image'>
+          <BaseImage image={{ src: img }} />
+        </div>
+        <div className='main-banner__text'>
           <h1 className='main-banner__title'>
-            Front-End Developer <br /> Petro Maiko
+            <span className='text1'>Front-End</span> <span className='text2'>Developer</span> <br /> <span className='name'>Petro <br /> Maiko.</span>
           </h1>
           <h2 className='main-banner__description'>
-            Building the best websites in the world.
+            Building the best websites<br /> in the world!!!
           </h2>
+
           <BaseButton className='main-banner__button'>
             SEND REQUEST
           </BaseButton>
-        </article>
-        <div className='main-banner__photo'>
-          <img
-            src={img}
-            alt='petyamaiko'
-            className='main-banner__photo-img'
-          />
-          <img
-            src={svg}
-            alt='code'
-            className='main-banner__photo-code'
-          />
         </div>
       </div>
     </section>
