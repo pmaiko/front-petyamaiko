c9cff6888b80752dd40c3a6b8084a91a3a40e0e4
petyamaiko <petyamaiko@gmail.com>
2023-02-18 19:01:00 +0200

    work chat video

--- src/components/shared/chat/ChatCallSpeaking.tsx
+++ src/components/shared/chat/ChatCallSpeaking.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from 'react'
-import { Peer } from 'peerjs'
+import { Peer, MediaConnection } from 'peerjs'
 import { useSocket } from '~/providers/SocketProvider'
 
 let options: object
@@ -21,13 +21,48 @@ if (process.env.NODE_ENV === 'development') {
 let peer: Peer
 
 const ChatCallSpeaking = () => {
-  const { socket, mySocketId, callInfo, peerId } = useSocket()
+  const {
+    socket,
+    mySocketId,
+    callInfo,
+    peerId,
+    onCancelCall
+  } = useSocket()
   const myVideo = useRef<HTMLVideoElement>(null)
   const partnerVideo = useRef<HTMLVideoElement>(null)
   const [hasMyVideo, setHasMyVideo] = useState(false)
   const [hasPartnerVideo, setHasPartnerVideo] = useState(false)
+  const [isVideo, setIsVideo] = useState(true)
+  const [isAudio, setIsAudio] = useState(false)
+
+  function stopStreamedVideo (nodeMyVideo?: HTMLVideoElement, nodePartnerVideo?: HTMLVideoElement) {
+    const el1 = nodeMyVideo || myVideo?.current
+    const el2 = nodePartnerVideo || partnerVideo?.current
+
+    const stream1 = el1?.srcObject as MediaStream
+    const stream2 = el2?.srcObject as MediaStream
+
+    if (stream1) {
+      stream1.getTracks().forEach((track) => {
+        track.stop()
+      })
+    }
+
+    if (stream2) {
+      stream2.getTracks().forEach((track) => {
+        track.stop()
+      })
+    }
+
+    // @ts-ignore
+    el1.srcObject = null
+    // @ts-ignore
+    el2.srcObject = null
+  }
 
   useEffect(() => {
+    const nodeMyVideo = myVideo.current as HTMLVideoElement
+    const nodePartnerVideo = partnerVideo.current as HTMLVideoElement
     peer = new Peer(options)
 
     peer.on('open', (id) => {
@@ -40,40 +75,76 @@ const ChatCallSpeaking = () => {
         to: callInfo.to === mySocketId ? callInfo.from : callInfo.to
       })
     })
+
+    return () => {
+      peer.disconnect()
+      peer.destroy()
+      stopStreamedVideo(nodeMyVideo, nodePartnerVideo)
+    }
   }, [])
+
+  const usersCommunication = (_stream: MediaStream = new MediaStream()) => {
+    const call = peer.call(peerId, _stream)
+
+    call.on('stream', (remoteStream) => {
+      setHasPartnerVideo(true)
+      addVideoStream(partnerVideo.current, remoteStream)
+    })
+
+    peer.on('call', (answer) => {
+      answer.answer(_stream)
+      answer.on('stream', (remoteStream) => {
+        setHasPartnerVideo(true)
+        addVideoStream(partnerVideo.current, remoteStream)
+      })
+    })
+  }
+
+  const onStream = async (video = true, audio = false) => {
+    setIsVideo(video)
+    setIsAudio(audio)
+
+    if (!isVideo && !isAudio) {
+      return
+    }
+
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: isVideo,
+        audio: isAudio
+      })
+
+      setHasMyVideo(true)
+      addVideoStream(myVideo.current, stream)
+
+      usersCommunication(stream)
+
+    } catch (error) {
+      stopStreamedVideo()
+      console.log(error)
+    }
+  }
 
   useEffect(() => {
     if (peerId) {
-      navigator.mediaDevices.getUserMedia({
-        // audio: true,
-        video: true
-      }).then((stream: MediaStream) => {
-        setHasMyVideo(true)
-        addVideoStream(myVideo.current, stream)
-
-        const call = peer.call(peerId, stream)
-
-        peer.on('call', (answer) => {
-          answer.answer(stream)
-          answer.on('stream', (remoteStream) => {
-            setHasPartnerVideo(true)
-            addVideoStream(partnerVideo.current, remoteStream)
-          })
-        })
-
-        call.on('stream', (remoteStream) => {
-          setHasPartnerVideo(true)
-          addVideoStream(partnerVideo.current, remoteStream)
-        })
-      })
+      onStream()
+      usersCommunication()
     }
   }, [peerId])
 
-  const addVideoStream = (video: any, stream: any) => {
-    video.srcObject = stream
+  const addVideoStream = (video: any, _stream: MediaStream) => {
+    video.srcObject = _stream
     video.addEventListener('loadedmetadata', () => {
       video.play()
     })
+  }
+
+  const toggleCamera = () => {
+    onStream(!isVideo)
+  }
+
+  const toggleAudio = () => {
+    onStream(isVideo, !isAudio)
   }
 
   return (
@@ -135,7 +206,16 @@ const ChatCallSpeaking = () => {
         </div>
       </div>
       <div className='chat-call-speaking__foot'>
-        <button className='chat-call__button chat-call__button_cansel'>
+        <button onClick={toggleCamera}>
+          Toggle camera
+        </button>
+        <button onClick={toggleAudio}>
+          Toggle Audio
+        </button>
+        <button
+          className='chat-call__button chat-call__button_cansel'
+          onClick={onCancelCall}
+        >
           <i className='fa-solid fa-xmark' />
         </button>
       </div>
